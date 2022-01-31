b960b999fc46c24ee0a499fc772c838070f0a576
Dipendra Pratap <58151564+dipdregan@users.noreply.github.com>
2022-01-30 23:13:02 -0800

    Update app.py

--- app.py
+++ app.py
@@ -256,5 +256,6 @@ if __name__ == "__main__":
 port = int(os.getenv("PORT", 5000))
 if __name__ == "__main__":
     host = '0.0.0.0'
-    port = 5000
+    #port = 5000
+    port = int(os.getenv("PORT", 5110))
     app.run(host=host, port=port)
